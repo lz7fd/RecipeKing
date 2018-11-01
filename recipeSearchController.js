486e5fff8cf8077b8d8af0483b4d7c87d8785415
kylestevenson <krs6y9@mail.missouri.edu>
2018-10-31 21:48:24 -0500

    Added ajax call to recipe search controller

--- recipeSearchController.js
+++ recipeSearchController.js
@@ -1,47 +1,37 @@
 var app = angular.module("recipeSearch", []);
-app.controller("recipeSearchCtrl", function ($scope) {
+app.controller("recipeSearchCtrl", function($scope, $http){
     $scope.ingredients = [];
     $scope.recipes = [];
-    $scope.showRecipeTable = false;
-    $scope.addIngredient = function () {
-        if (!$scope.addIn) {
+    $scope.message = "";
+    $scope.showRecipeTable =false;
+    $scope.addIngredient = function() {
+        if(!$scope.addIn){
             return;
         }
-        if ($scope.ingredients.indexOf($scope.addIn) === -1) {
+        if ($scope.ingredients.indexOf($scope.addIn) == -1){
             $scope.ingredients.push($scope.addIn);
             $scope.addIn = "";
-        } else {
+        }else {
             $scope.errorMsg = "Ingredient already added.";
         }
-    };
+    }
     $scope.removeIngredient = function (x) {
         $scope.errorMsg = "";
         $scope.ingredients.splice(x, 1);
-    };
-    $scope.recipeSearch = function () {
+    }
+    $scope.recipeSearch = function() {
         $scope.showRecipeTable = true;
         //Need to add Ajax call to PHP, send ingredients[], receive JSON -> display
+        $http.post("test.php").then
         $http({
-            url: 'recipe_king.php',
             method: 'POST',
-//            params : {'q':$scope.ingredients}
-            data: ingredients
-        }).then(function successCallback(response) {
-            obj = JSON.parse(response.data);
-        }, function errorCallback(response) {
-            $scope.error = response.statusText;
+            url: 'test.php',
+            data: $scope.ingredients,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }).then(function mySuccess(response) {
+            $scope.message = response.data;
+        }, function myError(response) {
+            $scope.message = response.statusText;
         });
-//        obj = JSON.parse(recipes);
-        $scope.recipes = {
-            "recipe1": {"label": obj.hits[1].label, "yield": obj.hits[1].yield, "calories": obj.hits[1].calories},
-            "recipe2": {"label": obj.hits[2].label, "yield": obj.hits[2].yield, "calories": obj.hits[2].calories},
-            "recipe3": {"label": obj.hits[3].label, "yield": obj.hits[3].yield, "calories": obj.hits[3].calories}
-        };
-        //For testing UI layout:
-//        $scope.recipes = {
-//            "recipe1": {"label":"Mashed Potatoes","yield":2,"calories":530},
-//            "recipe2": {"label":"Potato Stew","yield":5,"calories":730},
-//            "recipe3": {"label":"Baked Potato","yield":1,"calories":450}
-//        };
-    };
+    }
 });
